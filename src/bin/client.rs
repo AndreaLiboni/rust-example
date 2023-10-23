cf90f310869f3255f41acfc61f7a4e0b9e82aeae
AndreaLiboni <liboni.andrea@gmail.com>
2023-10-23 23:29:00 +0200

    add json structure

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,11 +1,12 @@
 use std::net::UdpSocket;
 
+
 fn main() -> std::io::Result<()> {
     {
         let socket = UdpSocket::bind("127.0.0.1:34254")?;
-        let msg = "ciao";
         let dest = "127.0.0.1:34253";
-        let _ = socket.send_to(msg.as_bytes(), &dest);
+        let msg = r#"{"name":"John","surname":"Wayne","age":43}"#;
+        socket.send_to(msg.as_bytes(), &dest);
     }
     Ok(())
 
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,33 @@
 use std::net::UdpSocket;
+use std::str;
+use serde_json::{Result, Value};
+use std::fs::OpenOptions;
 
 fn main() -> std::io::Result<()> {
     {
         let socket = UdpSocket::bind("127.0.0.1:34253")?;
         let mut buf = [0; 50];
-        let (amt, src) = socket.recv_from(&mut buf)?;
-        let msg = std::str::from_utf8(&buf[0..50]).expect("invalid utf-8 sequence");
-        println!("{}", msg);
+        /*let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open("/tmp/serde.json")?;*/
+        loop {
+            socket.recv_from(&mut buf)?;
+            let mut msg: &str = str::from_utf8(&buf).unwrap();
+            let mut actual_length = 0;
+            for c in msg.chars() {
+                if c == '\0' {break};
+                actual_length+=1;
+            }
+            let mut str_msg = String::from(msg);
+            str_msg.truncate(actual_length);
+            msg = &str_msg[..];
+            let json_data: Value = serde_json::from_str(&msg)?;
+            println!("Name: {}\nSurname: {}\nAge: {}", json_data["name"], json_data["surname"], json_data["age"]);
+            //let writer = json_data.to_writer_pretty();
+            //serde_json::to_writer(&file, &10)?;
+        }
     }
     Ok(())
 
