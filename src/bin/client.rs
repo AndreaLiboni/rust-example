33e2505ec70a8237968fe5e7512a6d4c33a3e79f
AndreaLiboni <liboni.andrea@gmail.com>
2023-10-24 18:06:16 +0200

    Aggiunto salvataggio/lettura da file e comando per eliminazione

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -5,7 +5,7 @@ fn main() -> std::io::Result<()> {
     {
         let socket = UdpSocket::bind("127.0.0.1:34254")?;
         let dest = "127.0.0.1:34253";
-        let msg = r#"{"name":"John","surname":"Wayne","age":43}"#;
+        let msg = r#"ADD:{"name":"John","surname":"Wayne","age":43}"#;
         socket.send_to(msg.as_bytes(), &dest);
     }
     Ok(())
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,32 +1,73 @@
+use serde::{Deserialize, Serialize};
 use std::net::UdpSocket;
-use std::str;
-use serde_json::{Result, Value};
 use std::fs::OpenOptions;
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Contatto {
+    name: String,
+    surname: String,
+    age: i32
+}
+impl PartialEq for Contatto {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name && self.surname == other.surname && self.age == other.age
+    }
+}
+
+fn byte_to_str(bytes: &[u8]) -> String{
+    let mut new_str: String = String::from("");
+    for c in bytes.iter() {
+        if *c == 0x0 {break};
+        new_str.push(*c as char);
+    }
+    new_str
+}
+
+fn save_to_file(rubrica: &Vec<Contatto>){
+    let file = OpenOptions::new()
+    .create(true)
+    .write(true)
+    .truncate(true)
+    .open("rubrica_data.json").unwrap();
+    let _ = serde_json::to_writer_pretty(&file, rubrica);
+}
+
+fn find_in_rubrica(rubrica: &Vec<Contatto>, contatto_to_find: &Contatto) -> i32{
+    let mut i = 0;
+    for cont in rubrica{
+        if cont == contatto_to_find {return i;}
+        i+=1;
+    }
+    return -1;
+}
 
 fn main() -> std::io::Result<()> {
     {
         let socket = UdpSocket::bind("127.0.0.1:34253")?;
         let mut buf = [0; 50];
-        /*let file = OpenOptions::new()
+        let file = OpenOptions::new()
         .create(true)
         .write(true)
-        .truncate(true)
-        .open("/tmp/serde.json")?;*/
+        .read(true)
+        .open("rubrica_data.json")?;
+        let mut rubrica: Vec<Contatto> = serde_json::from_reader(&file)?;
         loop {
             socket.recv_from(&mut buf)?;
-            let mut msg: &str = str::from_utf8(&buf).unwrap();
-            let mut actual_length = 0;
-            for c in msg.chars() {
-                if c == '\0' {break};
-                actual_length+=1;
+            let msg = byte_to_str(&buf);
+            let (cmd, data) = msg.split_once(':').unwrap();
+            let contatto_msg = serde_json::from_str(&data).unwrap();
+            if cmd == "ADD"{
+                rubrica.push(contatto_msg);
+                save_to_file(&rubrica);
+            } else if cmd == "DEL"{
+                let pos = find_in_rubrica(&rubrica, &contatto_msg);
+                if pos != -1{
+                    rubrica.remove(pos as usize);
+                    save_to_file(&rubrica);
+                }
             }
-            let mut str_msg = String::from(msg);
-            str_msg.truncate(actual_length);
-            msg = &str_msg[..];
-            let json_data: Value = serde_json::from_str(&msg)?;
-            println!("Name: {}\nSurname: {}\nAge: {}", json_data["name"], json_data["surname"], json_data["age"]);
-            //let writer = json_data.to_writer_pretty();
-            //serde_json::to_writer(&file, &10)?;
+            //let msg_Contatto: Contatto = serde_json::from_str(&msg)?;
+            //println!("Name: {}\nSurname: {}\nAge: {}", msg_Contatto.name, msg_Contatto.surname, msg_Contatto.age);
         }
     }
     Ok(())
